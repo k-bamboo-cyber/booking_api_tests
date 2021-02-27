677390e25cc9e6c35ee9bbe2462981c9e4d3b47a
k-bamboo-cyber <olesya1234562009@rambler.ru>
2021-02-27 11:07:59 +0300

    Add schema

--- model/schema.py
+++ model/schema.py
@@ -1,13 +1,47 @@
-from schema import Schema, And, Use, Optional, SchemaError
+import json
 
-schema = Schema([{
-    "firstname" : And(str, len),
-    "lastname" : And(str, len),
-    "totalprice" : And(Use(int), lambda n: 0 <= n),
-    "depositpaid" : bool,
-    "bookingdates" : {
-        "checkin" : full-date,
-        "checkout" : full-date"
-    },
-    Optional("additionalneeds") : str
-}])
+from schema import Schema, SchemaError
+
+post_booking_schema = Schema(
+    {
+        "bookingid": int,
+        "booking": {
+            "firstname": str,
+            "lastname": str,
+            "totalprice": int,
+            "depositpaid": bool,
+            "bookingdates": {
+                "checkin": str,
+                "checkout": str,
+            },
+            "additionalneeds": str,
+        },
+    }
+)
+
+get_booking_schema = Schema(
+    {
+        "firstname": str,
+        "lastname": str,
+        "totalprice": int,
+        "depositpaid": bool,
+        "bookingdates": {
+            "checkin": str,
+            "checkout": str,
+        },
+        "additionalneeds": str,
+    }
+)
+
+
+def is_validate(data: dict, schema: Schema) -> bool:
+    """
+    Validate json schema
+    :return: True if validation is successful else False
+    """
+    try:
+        data = json.loads(json.dumps(data, default=lambda o: o.__dict__))
+        schema.validate(data)
+        return True
+    except SchemaError:
+        return False
---
+++ tests/GetBookingTests/test_get_booking.py
@@ -0,0 +1,60 @@
+import pytest
+
+from model.schema import get_booking_schema, is_validate
+
+
+class TestGetBooking:
+    def test_get_booking(self, client, create_booking):
+        """
+        1. Add new booking
+        2. Get created booking by id
+        3. Check data and status
+        4. Validate schema
+        """
+        id_booking = create_booking.get("bookingid")
+        res = client.get_booking(id_booking)
+        assert res.json() == create_booking.get("booking")
+        assert is_validate(res.json(), get_booking_schema), "Check booking schema"
+
+    @pytest.mark.xfail(reason="принимает пустой айди")
+    def test_get_booking_empty_id(self, client, create_booking):
+        """
+        1. Add new booking
+        2. Search booking by empty id
+        3. Check data and status
+        4. Validate schema
+        """
+        res = client.get_booking("")
+        assert res.status_code == 400
+        assert res.json() == create_booking.get("booking")
+        assert is_validate(res.json(), get_booking_schema), "Check booking schema"
+
+    @pytest.mark.xfail(reason="несуществующий айди")
+    def test_get_booking_nonexist_id(self, client, create_booking):
+        """
+        1. Add new booking
+        2. Get created booking by id
+        3. Take non-exist booking
+        4. Check data and status
+        5. Validate schema
+        """
+        id_booking = create_booking.get("bookingid")
+        res = client.get_booking(int(id_booking) + 100000)
+        assert res.status_code == 400
+        assert res.json() == create_booking.get("booking")
+        assert is_validate(res.json(), get_booking_schema), "Check booking schema"
+
+    @pytest.mark.xfail(reason="200 при айди строкой")
+    def test_get_booking_by_string_id(self, client, create_booking):
+        """
+        1. Add new booking
+        2. Get created booking by id
+        3. Take booking by string id
+        4. Check data and status
+        5. Validate schema
+        """
+        id_booking = create_booking.get("bookingid")
+        res = client.get_booking(str(id_booking))
+        assert res.status_code == 400
+        assert res.json() == create_booking.get("booking")
+        assert is_validate(res.json(), get_booking_schema), "Check booking schema"
